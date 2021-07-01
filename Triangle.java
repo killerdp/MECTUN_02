41dfbe8590dfb772720166abf4be6bdd8756780b
chakka durgaprasad <86551396+killerdp@users.noreply.github.com>
2021-07-01 18:12:06 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,15 @@
+class Triangle
+{
+public static void main (String args[])
+{
+int n=5;
+for(int i=4;i>=0;i--)
+{
+for(int j=0;j<=i;j++)
+{
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
