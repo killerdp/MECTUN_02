a4b6bd43d6f66eb49304c5f026cc5bdafc673cad
chakka durgaprasad <86551396+killerdp@users.noreply.github.com>
2021-07-01 18:41:38 +0530

    Add files via upload

---
+++ Alphabatic.java
@@ -0,0 +1,15 @@
+class Sample
+{
+public static void main (String args[])
+{
+int alphabet=65;
+for(int i=0;i<=5;i++)
+{
+for(int j=0;j<i+1;j++)
+{
+System.out.print((char)(alphabet++));
+}
+System.out.println();
+}
+}
+}
